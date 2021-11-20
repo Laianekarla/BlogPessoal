4d12cc87a09fa477b899ead9a0f9353b21f9e1c8
Laiane <91036815+Laianekarla@users.noreply.github.com>
2021-11-19 21:02:32 -0300

    @Laianekarla [Task: CREATE deploy]

--- src/main/java/org/generation/blogPessoal/model/Usuario.java
+++ src/main/java/org/generation/blogPessoal/model/Usuario.java
@@ -5,27 +5,32 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
-import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 
+import io.swagger.annotations.ApiModelProperty;
+
 @Entity
-@Table (name = "tb_usuario")
+@Table(name = "tb_usuario")
 public class Usuario {
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
-	
-	@NotNull
-	@Size (min = 2, max = 100)
+
+	@NotBlank
+	@Size(min = 2, max = 100)
 	private String nome;
-	
-	@NotNull
-	@Size (min = 5, max = 100)
+
+	@ApiModelProperty(example = "email@email.com.br")
+	@NotBlank(message = "O atributo Usuário é Obrigatório!")
+	@Email(message = "O atributo Usuário deve ser um email válido!")
+	@Size(min = 5, max = 100)
 	private String usuario;
-	
-	@NotNull
-	@Size (min = 5, max = 100)
+
+	@NotBlank
+	@Size(min = 5, max = 100)
 	private String senha;
 
 	public long getId() {
